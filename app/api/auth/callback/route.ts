fd108f85b301d10d8e813beba0f0f3f6379e889d
noschist <sreenair1999@gmail.com>
2024-09-03 11:48:19 +0530

    refactored callback and env

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -2,58 +2,91 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import axios from "axios";
 
+// Extract environment variables
+const clientId = process.env.STRAVA_CLIENT_ID as string;
+const clientSecret = process.env.STRAVA_CLIENT_SECRET as string;
+const redirectUrl = process.env.APP_HOME_URL as string;
+const workingEnv = process.env.NODE_ENV;
+
+async function exchangeCodeForTokens(code: string) {
+    try {
+        const response = await axios.post(
+            "https://www.strava.com/oauth/token",
+            null,
+            {
+                params: {
+                    client_id: clientId,
+                    client_secret: clientSecret,
+                    grant_type: "authorization_code",
+                    code: code,
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        throw new Error("Error exchanging authorization code for tokens");
+    }
+}
+
+function setCookies(data: {
+    refresh_token: string;
+    access_token: string;
+    athlete: any;
+    expires_at: number;
+}) {
+    try {
+        const { refresh_token, access_token, athlete, expires_at } = data;
+        const expirationDate = new Date(expires_at * 1000);
+        const isSecure = workingEnv === "production";
+
+        cookies().set("rfresh_tkn", refresh_token, {
+            httpOnly: true,
+            secure: isSecure,
+            sameSite: "lax",
+            path: "/",
+            expires: expirationDate,
+        });
+
+        cookies().set("accss_tkn", access_token, {
+            httpOnly: true,
+            secure: isSecure,
+            sameSite: "lax",
+            path: "/",
+            expires: expirationDate,
+        });
+
+        cookies().set("user_details", JSON.stringify(athlete), {
+            path: "/",
+            expires: expirationDate,
+            sameSite: "lax",
+            httpOnly: true,
+            secure: isSecure,
+        });
+    } catch (error) {
+        throw new Error("Error setting cookies");
+    }
+}
+
 export async function GET(req: Request) {
-  const clientId = process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID as string;
-  const clientSecret = process.env.NEXT_PUBLIC_STRAVA_CLIENT_SECRET as string;
-  const { searchParams } = new URL(req.url);
-  const code = searchParams.get("code");
+    const { searchParams } = new URL(req.url);
+    const code = searchParams.get("code");
 
-  if (!code) {
-    return NextResponse.json(
-      { error: "Authorization code is missing" },
-      { status: 400 }
-    );
-  }
+    if (!code) {
+        return NextResponse.json(
+            { error: "Authorization code is missing" },
+            { status: 400 }
+        );
+    }
 
-  try {
-    const response = await axios.post(
-      "https://www.strava.com/oauth/token",
-      null,
-      {
-        params: {
-          client_id: clientId,
-          client_secret: clientSecret,
-          grant_type: "authorization_code",
-          code: code as string,
-        },
-      }
-    );
+    try {
+        const tokenData = await exchangeCodeForTokens(code);
+        setCookies(tokenData);
 
-    const { refresh_token, access_token, athlete, expires_at } = response.data;
-
-    cookies().set("rfresh_tkn", refresh_token, {
-      httpOnly: true,
-      secure: true,
-      path: "/",
-      expires: new Date(expires_at * 1000),
-    });
-    cookies().set("accss_tkn", access_token, {
-      httpOnly: true,
-      secure: true,
-      path: "/",
-      expires: new Date(expires_at * 1000),
-    });
-    cookies().set("user_details", JSON.stringify(athlete), {
-      path: "/",
-      expires: new Date(expires_at * 1000),
-    });
-
-    // const cleanUrl = new URL(req.url);
-    // cleanUrl.searchParams.delete("code");
-    return NextResponse.redirect(
-      process.env.NEXT_PUBLIC_STRAVA_HOME_URL as string
-    );
-  } catch (error) {
-    return NextResponse.json({ error: "Something happened" }, { status: 500 });
-  }
+        return NextResponse.redirect(redirectUrl);
+    } catch (error) {
+        return NextResponse.json(
+            { error: error || "Something went wrong" },
+            { status: 500 }
+        );
+    }
 }
